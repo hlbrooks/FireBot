408a50255f5a3cec9c10668aaa2983d6f1661879
hlbrooks <78882572+hlbrooks@users.noreply.github.com>
2021-12-12 08:50:19 -0800

    Add files via upload

---
+++ docs/task___extinguisher_8h.js
@@ -0,0 +1,4 @@
+var task___extinguisher_8h =
+[
+    [ "task_Extinguisher", "task___extinguisher_8h.html#a5c47f313f862eec6774850d580d20efb", null ]
+];
---
+++ docs/task___rotation___base_8cpp.js
@@ -0,0 +1,11 @@
+var task___rotation___base_8cpp =
+[
+    [ "AIN1", "task___rotation___base_8cpp.html#ab9e303f2afe757487549fb0bc5a7c4fb", null ],
+    [ "AIN2", "task___rotation___base_8cpp.html#add470e0e05c8ce17467a81d7087cba46", null ],
+    [ "PWMA", "task___rotation___base_8cpp.html#a3ea32653b4399f65cd8a9fbd3856a15d", null ],
+    [ "STBY", "task___rotation___base_8cpp.html#a3603e97fc1a6348a090d0ce8aaef627d", null ],
+    [ "task_Rotation_Base", "task___rotation___base_8cpp.html#abe76723d796e9908aecdf133ddd81f0e", null ],
+    [ "motor1", "task___rotation___base_8cpp.html#a17bd668b0b008c446aa5486f4f005bcd", null ],
+    [ "offsetA", "task___rotation___base_8cpp.html#a03fa10acd8ead0d5cc8d8fc47a5334d0", null ],
+    [ "ROTATION_BASE_PERIOD", "task___rotation___base_8cpp.html#a27ca47c624525e6f088abdbc3203719f", null ]
+];
